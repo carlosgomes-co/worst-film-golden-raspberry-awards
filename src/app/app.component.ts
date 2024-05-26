f5847879ba2b277bbfcb2dbf274f5892273319d6
Carlos Eduardo Gomes <carlitoshxcx@gmail.com>
2024-05-25 21:07:51 -0300

    ✨ feat(app): Add main styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "../shared/components/header/header.component";
+import { SidebarComponent } from "../shared/components/sidebar/sidebar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet />`
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HeaderComponent, SidebarComponent]
 })
-export class AppComponent {}
+export class AppComponent { }
--- src/shared/components/sidebar/sidebar.component.ts
+++ src/shared/components/sidebar/sidebar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from "../menu/menu.component";
 
 @Component({
-  selector: 'app-sidebar',
-  standalone: true,
-  imports: [],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+    selector: 'app-sidebar',
+    standalone: true,
+    templateUrl: './sidebar.component.html',
+    styleUrl: './sidebar.component.scss',
+    imports: [MenuComponent]
 })
 export class SidebarComponent {
 
